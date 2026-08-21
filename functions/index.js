de3f91e4bd5b25a37d80291d3b809c7e86ea2d77
Kaancodm <161161987+Kaancodm@users.noreply.github.com>
2026-08-21 02:41:37 +0200

    fix: export complete coaching callable surface

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const admin = require('firebase-admin');
+
 admin.initializeApp();
+
 const coaching = require('./coaching');
+
 exports.createCoachingRequest = coaching.createCoachingRequest;
 exports.acceptCoachingRequest = coaching.acceptCoachingRequest;
 exports.completeCoachingRequest = coaching.completeCoachingRequest;
