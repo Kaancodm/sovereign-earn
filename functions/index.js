9e2ea1f1f81a901ca3859c9a6016b2ea337daae0
Kaancodm <161161987+Kaancodm@users.noreply.github.com>
2026-08-26 20:30:21 +0200

    feat: fail closed for financial functions in closed beta

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,35 @@
 /**
  * Sovereign Earn – Cloud Functions (Node 22, Functions v2).
- * Zentraler Entrypoint für Coaching, WorkAI, Nutzerprofile und Postbacks.
+ * Zentraler Entrypoint für Build Advisor, Nutzerprofile und optionale Finanzpfade.
+ *
+ * Closed-beta safety: coaching/escrow, coins and offerwall postbacks are disabled
+ * unless FINANCIALS_ENABLED is explicitly set to "true" in the deployment runtime.
  */
 
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-const coaching = require("./coaching");
-exports.acceptCoachingRequest = coaching.acceptCoachingRequest;
-exports.completeCoachingRequest = coaching.completeCoachingRequest;
-exports.declineCoachingRequest = coaching.declineCoachingRequest;
-exports.cancelCoachingRequest = coaching.cancelCoachingRequest;
+const FINANCIALS_ENABLED = process.env.FINANCIALS_ENABLED === "true";
 
 const workai = require("./workai");
-exports.createCoachingRequest = workai.createCoachingRequest;
 exports.askBuildAdvisor = workai.askBuildAdvisor;
-exports.updateSenseiProfile = workai.updateSenseiProfile;
 exports.healthCheck = workai.healthCheck;
 
 const users = require("./users");
 exports.ensureUserProfile = users.ensureUserProfile;
 exports.requestAccountDeletion = users.requestAccountDeletion;
 
-const postbacks = require("./postbacks");
-exports.pollfishPostbackV2 = postbacks.pollfishPostbackV2;
-exports.bitlabsPostbackV2 = postbacks.bitlabsPostbackV2;
+if (FINANCIALS_ENABLED) {
+  const coaching = require("./coaching");
+  exports.acceptCoachingRequest = coaching.acceptCoachingRequest;
+  exports.completeCoachingRequest = coaching.completeCoachingRequest;
+  exports.declineCoachingRequest = coaching.declineCoachingRequest;
+  exports.cancelCoachingRequest = coaching.cancelCoachingRequest;
+
+  exports.createCoachingRequest = workai.createCoachingRequest;
+  exports.updateSenseiProfile = workai.updateSenseiProfile;
+
+  const postbacks = require("./postbacks");
+  exports.pollfishPostbackV2 = postbacks.pollfishPostbackV2;
+  exports.bitlabsPostbackV2 = postbacks.bitlabsPostbackV2;
+}
